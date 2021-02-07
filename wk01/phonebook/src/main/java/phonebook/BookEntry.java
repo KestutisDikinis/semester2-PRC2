09464a82514adbfc3554fb8a7d1b0d3be79def84
www-data <www-data@7e224b12-4b88-424b-9eb2-3a2de760ecd3>
2021-02-07 21:08:17 +0000

    'import week wk01 tasks'

---
+++ wk01/phonebook/src/main/java/phonebook/BookEntry.java
@@ -0,0 +1,14 @@
+package phonebook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author urs
+ */
+
+public class BookEntry {
+    //TODO
+    
+}
---
+++ wk01/phonebook/src/test/java/phonebooktest/BookEntryTest.java
@@ -0,0 +1,9 @@
+package phonebooktest;
+
+/**
+ *
+ * @author Pieter van den Hombergh {@code p.vandehombergh@gmail.com}
+ */
+public class BookEntryTest {
+    
+}
