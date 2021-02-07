787d515adabe0685a9f8934d30a4bb0a136bfdc2
www-data <www-data@7e224b12-4b88-424b-9eb2-3a2de760ecd3>
2021-02-07 21:08:16 +0000

    'import week wk01 tasks'

---
+++ trunk/wk01/firstcontact/src/test/java/firstcontact/GreetingTest.java
@@ -0,0 +1,18 @@
+package firstcontact;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ *
+ * @author Pieter van den Hombergh {@code p.vandehombergh@gmail.com}
+ */
+public class GreetingTest {
+
+    @Test
+    void firstContact() {
+        //TODO implement firstContact
+        fail("firstContact completed, Your know waht to do");
+    }
+
+}
