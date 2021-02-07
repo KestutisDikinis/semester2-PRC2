a5b2621c6d50529fe7bb10b91682a25afd9aa891
www-data <www-data@7e224b12-4b88-424b-9eb2-3a2de760ecd3>
2021-02-07 21:08:17 +0000

    'import week wk01 tasks'

---
+++ wk01/simplequeue/src/test/java/simplequeue/SimpleQueueTest.java
@@ -0,0 +1,16 @@
+package simplequeue;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ *
+ * @author Pieter van den Hombergh {@code p.vandehombergh@gmail.com}
+ */
+public class SimpleQueueTest {
+
+    //TODO
+    
+}
