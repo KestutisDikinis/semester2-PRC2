20c288c1c2c8de95f9cf8d362aaed88172b9058e
www-data <www-data@7e224b12-4b88-424b-9eb2-3a2de760ecd3>
2021-02-07 21:08:17 +0000

    'import week wk01 tasks'

---
+++ trunk/wk01/simplequeue/src/main/java/simplequeue/Node.java
@@ -0,0 +1,11 @@
+package simplequeue;
+
+/**
+ *
+ * @author Pieter van den Hombergh {@code p.vandehombergh@gmail.com}
+ */
+public class Node<E> {
+
+    //TODO
+    
+}
---
+++ trunk/wk01/simplequeue/src/main/java/simplequeue/SimpleQueue.java
@@ -0,0 +1,10 @@
+package simplequeue;
+
+/**
+ *
+ * @author Pieter van den Hombergh {@code p.vandehombergh@gmail.com}
+ */
+public class SimpleQueue<T> {
+    //TODO
+    
+}
