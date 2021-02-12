5432430268e0776f81b0e69f5459160f1b1e16dd
3969630 <3969630@7e224b12-4b88-424b-9eb2-3a2de760ecd3>
2021-02-12 12:07:12 +0000

    working on phonebook

---
+++ trunk/wk01/phonebook/src/main/java/phonebook/Person.java
@@ -0,0 +1,33 @@
+package phonebook;
+
+public class Person {
+
+    String name;
+    String address;
+    String phoneNumber;
+
+    public Person(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
--- trunk/wk01/phonebook/src/test/java/phonebooktest/PhonebookTest.java
+++ trunk/wk01/phonebook/src/test/java/phonebooktest/PhonebookTest.java
@@ -10,5 +10,51 @@ import org.junit.jupiter.api.Test;
  */
 public class PhonebookTest {
     //TODO write tests
-    
+    Phonebook phonebook = new Phonebook();
+
+    @Test
+    void EntryNotFoundTest(){
+        assertThat(phonebook.searchByName("Jukka"))
+                .as("Person is not present in the list")
+                .isNull();
+    }
+
+    @Test
+    void addContactTest(){
+        phonebook.addEntry("Pekka", "040-123456");
+        assertThat(phonebook.searchByName("Pekka").getPhoneNumber())
+                .as("An added person, phone number should be found")
+                .contains("040-123456");
+    }
+
+    @Test
+    public void phonebookSearchByNumber() {
+        phonebook.addEntry("Pekka", "040-123456");
+        assertThat(phonebook.searchByNumber("040-123456").getName())
+                .as("search by number should return person's name")
+                .contains("Pekka");
+    }
+
+    @Test
+    public void phonebookSearchByNumberNotFound() {
+        assertThat(phonebook.searchByNumber("02-444123"))
+                .as("search for number not present should return null")
+                .isNull();
+    }
+
+    @Test
+    public void phonebookSearchAddress(){
+        phonebook.addEntry( "Pekka", "040-123456" );
+        phonebook.addAddress( "Pekka", "Hulsterweg 6, Venlo" );
+        assertThat(phonebook.searchByName( "Pekka" ).getAddress())
+                .as("after add, parts of the address required")
+                .contains( "Hulsterweg 6", "Venlo" );
+    }
+
+    @Test
+    public void phonebookDeletesEntry() {
+        phonebook.addEntry("Pekka", "040-123456");
+        phonebook.deleteEntry("Pekka");
+        assertThat(phonebook.searchByName("Pekka")).isNull();
+    }
 }
