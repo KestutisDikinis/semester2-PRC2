e7baaf3f0d0d2e4f3416a1ec9918fdca5b91ad3f
3969630 <3969630@7e224b12-4b88-424b-9eb2-3a2de760ecd3>
2021-02-22 16:51:36 +0000

    working on beeeeeeer

--- trunk/wk01/simplequeue/src/main/java/simplequeue/Node.java
+++ trunk/wk01/simplequeue/src/main/java/simplequeue/Node.java
@@ -7,5 +7,11 @@ package simplequeue;
 public class Node<E> {
 
     //TODO
-    
+    E data;
+    Node next;
+  
+    public Node(E data) {
+        this.data = data;
+        this.next = null;
+    }
 }
--- trunk/wk01/simplequeue/src/main/java/simplequeue/SimpleQueue.java
+++ trunk/wk01/simplequeue/src/main/java/simplequeue/SimpleQueue.java
@@ -1,10 +1,17 @@
 package simplequeue;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author Pieter van den Hombergh {@code p.vandehombergh@gmail.com}
  */
 public class SimpleQueue<T> {
     //TODO
-    
+    LinkedList<Node> list = new LinkedList<>();
+
+
+    public boolean isEmpty() {
+        return list.getFirst() == null && list.getLast() == null;
+    }
 }
