321909922dd0f079a106e5a16125cb9c5ac0a723
3969630 <3969630@7e224b12-4b88-424b-9eb2-3a2de760ecd3>
2021-02-28 17:20:08 +0000

    working on beeeeeeer

--- wk01/simplequeue/src/test/java/simplequeue/SimpleQueueTest.java
+++ wk01/simplequeue/src/test/java/simplequeue/SimpleQueueTest.java
@@ -2,6 +2,8 @@ package simplequeue;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
@@ -14,7 +16,53 @@ public class SimpleQueueTest {
 
     @Test
     void queueIsGeneric(){
+        SimpleQueue<String> queue = new SimpleQueue<>();
+        SimpleQueue<Integer> queue2 = new SimpleQueue<>();
+    }
 
+    @Test
+    void emptyTest(){
+        SimpleQueue<String> queue = new SimpleQueue<>();
+        assertThat(queue.isEmpty()).isTrue();
+    }
+
+    @Test
+    void peektest(){
+        SimpleQueue<String> queue = new SimpleQueue<>();
+        SimpleQueue<Integer> queue2 = new SimpleQueue<>();
+        String expectedString = "Kestutis";
+        queue.put(expectedString);
+        queue2.put(200);
+        assertThat(queue.peek()).isSameAs( expectedString );
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(queue.peek()).isEqualTo(expectedString);
+            softAssertions.assertThat(queue2.peek()).isEqualTo(200);
+        });
+    }
+
+    @Test
+    void takeTest(){
+        SimpleQueue<String> queue = new SimpleQueue<>();
+        String expected = "Kestutis";
+        queue.put(expected);
+        String taken = queue.take();
+
+        assertThat(taken).isEqualTo(expected);
+    }
+
+    @Test
+    void multipleTest() {
+        SimpleQueue<String> queue = new SimpleQueue<>();
+        String[] values= { "Hello", "World", "And", "Some", "Bla", "Di", "Bla" };
+        for(String value : values){
+            queue.put(value);
+        }
+        ArrayList<String> returns = new ArrayList<>();
+        while (!queue.isEmpty()){
+            returns.add(queue.take());
+        }
+
+        assertThat(returns).containsExactly(values);
     }
     
 }
